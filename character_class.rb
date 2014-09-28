f1fbfe894fc22090cff8db41f36f8847ccef0fb0
Holly Moody <hollymoody@charter.net>
2014-09-28 17:14:38 -0400

    Fill out character class attributes

--- character_class.rb
+++ character_class.rb
@@ -8,40 +8,40 @@ class CharacterClass
 ABILITY_DEFAULTS = {}
 CHARACTER_CLASSES =
   { "Cleric" => {
-      description: "A priestly champion who wields divine magic in service of a higher power",
+      description: "A priestly champion who wields divine magic in service of a higher power.",
       starting_wealth: "",
-      hit_die: "d8",
-      primary_ability: "Wisdom",
-      saving_throw: "Wisdom, Charisma",
-      armor_proficiency: {},
-      weapon_proficiency: {}
+      hit_die: 8,
+      primary_ability: ["Wisdom"],
+      saving_throw_proficiencies: ["Wisdom", "Charisma"],
+      armor_proficiency: ["Light Armor", "Medium Armor", "Shield"],
+      weapon_proficiency: ["Simple Melee Weapons"]
       },
     "Fighter" => {
-      description: "",
+      description: "A master of martial combat, skilled with a variety of weapons and armor",
       starting_wealth: "",
-      hit_die: "",
-      primary_ability: {},
-      saving_throw: {},
-      armor_proficiency: {},
-      weapon_proficiency: {}
+      hit_die: 10,
+      primary_ability: ["Strength", "Dexterity"],
+      saving_throw_proficiencies: ["Strength", "Constitution"],
+      armor_proficiency: ["Light Armor", "Medium Armor", "Heavy Armor", "Shield"],
+      weapon_proficiency: ["Simple Melee Weapons", "Simple Ranged Weapons", "Martial Melee Weapons", "Martial Ranged Weapons"]
       },
     "Rogue" => {
-      description: "",
+      description: "A scoundrel who uses stealth and trickery to overcome obstacles and enemies.",
       starting_wealth: "",
-      hit_die: "",
-      primary_ability: {},
-      saving_throw: {},
-      armor_proficiency: {},
-      weapon_proficiency: {}
+      hit_die: 8,
+      primary_ability: ["Dexterity"],
+      saving_throw_proficiencies: ["Dexterity", "Intelligence"],
+      armor_proficiency: ["Light Armor"],
+      weapon_proficiency: ["Simple Melee Weapons", "Hand Crossbows", "Longswords", "Rapiers", "Shortswords"]
       },
     "Wizard" => {
-      description: "",
+      description: "A scholarly magic-user capable of manipulating the structures of reality.",
       starting_wealth: "",
-      hit_die: "",
-      primary_ability: {},
-      saving_throw: {},
-      armor_proficiency: {},
-      weapon_proficiency: {}
+      hit_die: 6,
+      primary_ability: ["Intelligence"],
+      saving_throw_proficiencies: ["Intelligence", "Wisdom"],
+      armor_proficiency: ["Light Armor", "Medium Armor", "Shield", "Simple Melee Weapons"],
+      weapon_proficiency: ["Daggers", "Darts", "Sling", "Quarterstaff", "light Crossbow"]
     }
 }
 
