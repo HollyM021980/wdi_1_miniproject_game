958c5dcb0fe9ebee90e9d51bd77e069be3e19acd
Holly Moody <hollymoody@charter.net>
2014-09-28 18:16:26 -0400

    Add dwarf name data

--- dwarf.rb
+++ dwarf.rb
@@ -2,6 +2,11 @@ require 'pry'
 
 # This describes all of the common attributes of a Dungeons
 # and Dragons character
+
+MALE_NAMES = ["Adrik", "Alberich", "Baern", "Barendd", "Brottor", "Bruenor", "Dain", "Darrak", "Delg", "Eberk", "Einkil", "Fargrim", "Flint", "Gardain", "Harbek", "Kildrak", "Morgran", "Orsik", "Oskar", "Rangrim", "Rurik", "Taklinn", "Thoradin", "Thorin", "Tordek", "Traubon", "Travok", "Ulfgar", "Veit", "Vondal"]
+FEMALE_NAMES = ["Amber", "Artin", "Audhild", "Bardryn", "Dagnal", "Diesa", "Eldeth", "Falkrunn", "Finellen", "Gunnloda", "Gurdis", "Helja", "Hlin", "Kathra", "Kristryd", "Ilde", "Liftrasa", "Mardred", "Riswynn", "Sannl", "Torbera", "Torgga", "Vistra"]
+CLAN_NAMES = ["Balderk", "Battlehammer", "Brawnanvil", "Dankil", "Fireforge", "Frostbeard", "Gorunn", "Holderhek", "Ironfist", "Loderr", "Lutgehr", "Rumnaheim", "Strakeln", "Torunn", "Ungart"]
+
 class Dwarf < CharacterRace
 
 end
